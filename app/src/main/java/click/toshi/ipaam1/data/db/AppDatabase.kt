76a125dddee2b1d99428614e39e894986cf211fb
toshi <toshi@toshi.click>
2018-07-11 12:29:00 +0900

    add issueテーブル

--- app/src/main/java/click/toshi/ipaam1/data/db/AppDatabase.kt
+++ app/src/main/java/click/toshi/ipaam1/data/db/AppDatabase.kt
@@ -3,9 +3,11 @@ package click.toshi.ipaam1.data.db
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverters
+import click.toshi.ipaam1.data.db.dao.IssueDao
 import click.toshi.ipaam1.data.db.dao.SessionDao
 import click.toshi.ipaam1.data.db.dao.SessionSpeakerJoinDao
 import click.toshi.ipaam1.data.db.dao.SpeakerDao
+import click.toshi.ipaam1.data.db.entity.IssueEntity
 import click.toshi.ipaam1.data.db.entity.SessionEntity
 import click.toshi.ipaam1.data.db.entity.SessionSpeakerJoinEntity
 import click.toshi.ipaam1.data.db.entity.SpeakerEntity
@@ -15,7 +17,8 @@ import click.toshi.ipaam1.data.db.entity.mapper.Converters
         entities = [
             (SessionEntity::class),
             (SpeakerEntity::class),
-            (SessionSpeakerJoinEntity::class)
+            (SessionSpeakerJoinEntity::class),
+            (IssueEntity::class)
         ],
         version = 1
 )
@@ -24,4 +27,5 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionDao
     abstract fun speakerDao(): SpeakerDao
     abstract fun sessionSpeakerDao(): SessionSpeakerJoinDao
+    abstract fun issueDao(): IssueDao
 }
---
+++ model/src/main/java/click/toshi/ipaam1/model/Issue.kt
@@ -0,0 +1,6 @@
+package click.toshi.ipaam1.model
+
+data class Issue(
+        val id: String,
+        val name: String
+)
